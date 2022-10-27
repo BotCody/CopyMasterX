929bbfd3c09ac8c6e05c3fc3e13bc9671ef7dcd3
shreecog <sreedhara.chandra@cognologix.com>
2022-10-27 06:54:14 +0530

    Initial commit

---
+++ src/main/java/com/solarwinds/testmultiexchangestreamer/TestMultiExchangeStreamerApplication.java
@@ -0,0 +1,13 @@
+package com.solarwinds.testmultiexchangestreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMultiExchangeStreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMultiExchangeStreamerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solarwinds/testmultiexchangestreamer/TestMultiExchangeStreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solarwinds.testmultiexchangestreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMultiExchangeStreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
